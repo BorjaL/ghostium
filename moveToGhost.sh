2078488cee57fc15a9eeb3d7aa6ecec23b651057
BorjaL <borja.fpillado@gmail.com>
2016-02-14 13:13:55 +0100

    Changes the cover logo size and it adds different colors to the cover image

--- moveToGhost.sh
+++ moveToGhost.sh
@@ -1,3 +1,7 @@
+grunt dryrun
+
 rm -Rf ../ghost/themes/ghostium
 mkdir ../ghost/themes/ghostium
 cp -R ../ghostium/* ../ghost/themes/ghostium
+
+docker restart personal-blog
---
+++ src/assets/js/coverColor.js
@@ -0,0 +1,4 @@
+var color = ['#550033','#0099ff','#003355','#00a172','#a800ff','#00d2ff','#7e00ff','#960058','#cb1831','#6e1160','#2a0f6f','#1aecae', '#04c1be'][Math.floor(Math.random()*13)];
+
+
+document.getElementsByClassName('cover-image')[0].style.backgroundColor = color;
